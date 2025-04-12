9bfa7df90da75cdf26f9849e7947a104a86b7222
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2025-04-12 19:38:28 +0200

    1st part of the landing

--- packages/frontend/src/components/Header/Header.tsx
+++ packages/frontend/src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
-import logo from '../../assets/images/logoWebV1Light.png';
+import logo from '@/assets/images/logoWebV1Light.png';
 import {Button} from "@/components/ui/button.tsx";
 
 export default function Header() {
     return (
-        <header className="flex items-center justify-between w-full p-4 bg-white">
+        <header className="flex items-center justify-between w-full p-4 bg-white h-[64px]">
             <div className="flex items-center">
                 <img
                     src={logo}
@@ -11,8 +11,8 @@ export default function Header() {
                     className="max-w-[150px] h-auto"
                 />
                 <div className="flex ml-16">
-                    <h1 className="text-black hover:underline cursor-pointer">Pourquoi nous</h1>
-                    <h1 className="text-black ml-8 hover:underline cursor-pointer">Voir votre quartier</h1>
+                    <h1 className="hover:underline cursor-pointer">Pourquoi nous</h1>
+                    <h1 className="ml-8 hover:underline cursor-pointer">Voir votre quartier</h1>
                 </div>
             </div>
             <Button className="bg-[#ED5C3B] text-white hover:bg-[#DF5839] font-bold">
--- packages/frontend/src/containers/LandingPage/LandingPage.tsx
+++ packages/frontend/src/containers/LandingPage/LandingPage.tsx
@@ -1,9 +1,30 @@
 import Header from "@/components/Header/Header.tsx";
+import neighborhood from "../../assets/images/landing_page_neighborhood.png";
+import "./landing-page.css"
+import {Button} from "@/components/ui/button.tsx";
 
 export default function LandingPage() {
     return (
-        <div className={'flex items-center justify-center flex-col h-screen w-screen bg-background'}>
+        <div className={'flex items-center justify-center flex-col w-screen bg-background'}>
             <Header/>
+            <div className='h-[calc(100vh-64px)] w-full flex items-center justify-center flex-col relative'>
+                <div className="w-full flex items-center justify-center flex-row">
+                    <div className={'w-[50%]'}>
+                        <p className={'text-[48px]'}>Découvrez ce qu’est vraiment une vie de <span
+                            className={'text-[#ED5C3B] font-bold'}>quartier</span>
+                        </p>
+                        <p className={"text-[24px]"}>
+                            Voisin comme cochon permet de
+                            <span className={"important-word"}>simplifier</span> l’entraide et
+                            l’échange entre les membres d’un quartier</p>
+                    </div>
+                    <img src={neighborhood}/>
+                </div>
+                <Button
+                    className={"absolute left-[50%] bottom-[64px] -translate-x-1/2 bg-[#ED5C3B] hover:bg-[#DF5839] font-bold"}>
+                    CRÉER MON QUARTIER
+                </Button>
+            </div>
         </div>
     );
 }
