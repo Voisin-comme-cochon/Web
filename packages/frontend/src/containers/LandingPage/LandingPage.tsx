eada90aec1f93e5b1813d6fde41cefad64481f46
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2025-04-12 16:31:05 +0200

    init archi + header

---
+++ packages/frontend/src/containers/LandingPage/LandingPage.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header/Header.tsx";
+
+export default function LandingPage() {
+    return (
+        <div className={'flex items-center justify-center flex-col h-screen w-screen bg-background'}>
+            <Header/>
+        </div>
+    );
+}
