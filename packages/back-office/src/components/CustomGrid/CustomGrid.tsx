7059ab3cbe99cf06dc4d799c82f52909449c7c23
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2025-05-22 18:49:15 +0200

    Update CustomGrid.tsx

--- packages/back-office/src/components/CustomGrid/CustomGrid.tsx
+++ packages/back-office/src/components/CustomGrid/CustomGrid.tsx
@@ -14,6 +14,7 @@ export interface CustomGridProps {
         formatter?: (cell: string | number) => React.JSX.Element;
     }
     >;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
     data: any;
     options?: Omit<GridConfig, 'columns' | 'data'>;
 }
