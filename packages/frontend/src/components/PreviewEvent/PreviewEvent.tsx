84343b86a7037b7b22c5cb05fb702b7588c92deb
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2025-05-29 17:44:48 +0200

    Update PreviewEvent.tsx

--- packages/frontend/src/components/PreviewEvent/PreviewEvent.tsx
+++ packages/frontend/src/components/PreviewEvent/PreviewEvent.tsx
@@ -3,12 +3,12 @@ import { UserModel } from '@/domain/models/user.model.ts';
 
 export default function PreviewEvent({ event, user }: { event: EventModel; user: UserModel }) {
     return (
-        <div className="relative rounded-2xl max-w-80 w-full overflow-hidden shadow-lg bg-gray-100">
-            <img src={event.photo} alt={event.name} className="w-full h-28 object-cover" />
+        <div className="relative rounded-2xl max-w-80 h-60 w-full overflow-hidden shadow-lg bg-gray-100">
+            <img src={event.photo} alt={event.name} className="w-full h-40 object-cover" />
             <div className="absolute bottom-0 left-0 w-full bg-white rounded-b-2xl flex flex-col gap-2 p-4 z-10">
                 <div className="flex flex-row justify-between items-center">
                     <h1 className="text-lg font-bold truncate">{event.name}</h1>
-                    <p className="text-sm text-gray-600">Le {new Date(event.dateStart).toLocaleDateString()}</p>
+                    <p className="text-sm text-gray-600">{new Date(event.dateStart).toLocaleDateString()}</p>
                 </div>
                 <div className="flex flex-row justify-between items-center">
                     <p className="text-gray-600 text-sm">
